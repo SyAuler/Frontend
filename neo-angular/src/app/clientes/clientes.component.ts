788a61078b46b8ecd96e0dda8e41bc0a15f0ffea
Simone Auler <auler.simone@gmail.com>
2020-04-08 20:59:49 -0300

    - criado projeto angular e instalado o bootstrap 4 - mapeado o get com a URL da lista de clientes para exibir os dados no navegador

---
+++ neo-angular/src/app/clientes/clientes.component.ts
@@ -0,0 +1,26 @@
+
+import { Component, OnInit } from '@angular/core';
+
+import { RoutesService } from '../routes.service';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.scss']
+})
+export class ClientesComponent implements OnInit {
+
+  clientes: Array<any>;
+
+  constructor(private routesService: RoutesService) { }
+
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar() { /* instancia a classe routes.service.ts */
+    this.routesService.listar()
+      .subscribe(dados => this.clientes = dados);
+  }
+
+}
